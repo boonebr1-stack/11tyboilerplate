a0bb04c606000fb3df56fbc6745e476926491dae
boonebr1-stack <boonebr1@msu.edu>
2025-11-03 04:37:08 +0000

    git add .

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,22 @@
 
 // The export statement makes these settings available to other files in 11ty
+Lab Requirements
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
+  // Shortcode: "Never leave home without it"
+  eleventyConfig.addShortcode("tagline", function() {
+    return `<p class="tagline">Never leave home without it.</p>`;
+  });
+
+  // Allow copying of styles and images
   eleventyConfig.addPassthroughCopy("styles");
+  eleventyConfig.addPassthroughCopy("images");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 };
